163358400161a746ec7fd09431f18109f62b9bd9
Marcus <marcus.frontdev@gmail.com>
2024-03-06 18:32:23 +0100

    create new component list and details and add cocktail exemple

---
+++ src/app/cocktail-list/cocktail-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Cocktail } from '../interfaces/cocktail.interface';
+
+@Component({
+  selector: 'app-cocktail-list',
+  templateUrl: './cocktail-list.component.html',
+  styleUrls: ['./cocktail-list.component.scss'],
+})
+export class CocktailListComponent {
+  cocktails: Cocktail[] = [
+    {
+      name: 'Mojito',
+      img: 'https://www.hangoverweekends.co.uk/uploads/images/mojito.jpg',
+      description:
+        'The Mojito complimenting summer perfectly with a fresh minty taste. The mixture of white rum, mint, lime juice, sugar and soda water is crisp and clean with a relatively low alcohol content, the soda water can be replaced with sprite or 7-up. When preparing a mojito always crush the mint leaves as opposed to dicing to unlock oils that will assist with enhancing the minty flavour.',
+    },
+  ];
+}
